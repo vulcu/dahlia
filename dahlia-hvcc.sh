431910baf797a16ec64407d425c1df9a6b19e458
Winry Litwa-Vulcu <19890187+vulcu@users.noreply.github.com>
2023-03-04 19:58:30 -0800

    build(hvcc): change `echo` to `printf` and add venv activate/deactivate

--- dahlia-hvcc.sh
+++ dahlia-hvcc.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "Cleaning up previous builds..."
+printf "\nCleaning up previous builds...\n"
 rm -r src/ir && rm -r src/hv && rm -r src/c
-echo "Building Dahlia via hvcc..." 
+
+printf "Building Dahlia via hvcc...\n"
+source ./.venv/Scripts/activate 
 hvcc ./pd/main.pd  -o ./src/ -n dahlia -p lib/heavylib lib/heavylib/hv.filters lib/heavylib/hv.osc --copyright "Winry Litwa-Vulcu 2021-2023 and Licensed under GPL-3.0"
-echo "Build Completed! If there are no errors above, the generated source files are located in /src"
+deactivate
+
+printf "Build Completed! If there are no errors above, the generated source files are located in /src\n"
