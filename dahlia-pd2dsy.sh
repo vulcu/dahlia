e4206a4659a07927c3b210b35c242f2d87c989de
Winry Litwa-Vulcu <19890187+vulcu@users.noreply.github.com>
2023-03-05 21:03:23 -0800

    build(pd2dsy): add `pd_filename` to shell script to replace hardcoded name

--- dahlia-pd2dsy.sh
+++ dahlia-pd2dsy.sh
@@ -5,21 +5,23 @@ dahlia_root=$PWD
 board=pod;
 ram=speed;
 rom=size;
-output_directory="../../src/";
+pd_filename="main"
+output_directory="../../src";
 search_paths="../heavylib/ -p ../heavylib/hv.filters -p ../heavylib/hv.osc";
 libdaisy_dir="../../lib/pd2dsy/libdaisy"
 no_build="--no-build";  # use "--no-build" to prevent automatic building and flashing after hvcc generation
 
 printf "\nCleaning up previous builds...\n"
-rm -r src/main
+rm -r src/$pd_filename
 
-printf "Building Dahlia via hvcc...\n" 
+printf "Building Dahlia via pd2dsy...\n" 
 cd lib/pd2dsy
 source ./pd_env/Scripts/activate
-python pd2dsy.py ../../pd/main.pd -b $board --ram $ram --rom $rom -d $output_directory -p $search_paths $no_build
+python pd2dsy.py ../../pd/$pd_filename.pd -b $board --ram $ram --rom $rom -d $output_directory -p $search_paths $no_build
 deactivate
 
-cd $output_directory/main
+printf "Building Heavy source with make and attempting upload via DFU...\n" 
+cd $output_directory/$pd_filename
 sed -i.bak -e "s|.*LIBDAISY_DIR = .*|LIBDAISY_DIR = $libdaisy_dir|" Makefile
 rm Makefile.bak
 make && make program-dfu
